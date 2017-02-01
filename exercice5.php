f2fcbe4950484502e1ab1da9bc177c8388c41c6c
JonathanMoroy <jonathan.moroy@gmail.com>
2017-02-01 09:23:56 +0100

    Caractères inutiles effacés.

--- exercice5.php
+++ exercice5.php
@@ -1,9 +1,9 @@
 <?php
 function dbz($chaine1, $chaine2)
 {
-  return ($chaine1.$chaine2);
+  return $chaine1 . $chaine2;
 }
-$ssj = "Vegeta";
-$ssj2 = "Goku";
+$ssj = 'Vegeta';
+$ssj2 = 'Goku';
 echo dbz($ssj, $ssj2);
 ?>
